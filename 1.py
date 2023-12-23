645b0df7b88d87205913c932ff42ebd94acec55e
odik8 <107572344+odik8@users.noreply.github.com>
2023-12-23 11:05:42 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def main():
-    data = [
-        ('Команда1', 15),
-        ('Команда2', 20),
-        ('Команда3', 30),
-    ]
-
-    sorted_data = sorted(data, key=lambda x: x[1], reverse=True)
+def check_places(list_of_teams, places)
+    sorted_teams = sorted(list_of_teams, key=lambda x: list_of_teams[x], reverse=True)
     return sorted_data
 
 
 if __name__ == "__main__":
-    print(main())
+    list_of_teams = ["Команда 1", "Команда 2", "Команда 3",]
+    print(check_places()
