1c38021c7ceeb0fb054d0ed1cb27a2652d3af8ed
odyssey <odissey.meltonyan@gmail.com>
2023-12-26 18:32:22 +0300

    fix 2

--- 1.py
+++ 1.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def main():
-    data = [
-        ('Команда1', 15),
-        ('Команда2', 20),
-        ('Команда3', 30),
-    ]
+def check_positions(teams, points):
+    sorted_teams = sorted(zip(teams, points), key=lambda x: x[1], reverse=True)
 
-    sorted_data = sorted(data, key=lambda x: x[1], reverse=True)
-    return sorted_data
+    sorted_teams = [t[0] for t in sorted_teams]
 
+    if sorted_teams == teams:
+        return "Команды перечислены в соответствии с их позициями в чемпионате."
+    else:
+        return "Команды перечислены не в соответствии с их позициями в чемпионате."
 
 
 if __name__ == "__main__":
-    print(main())
+    print(check_positions(["Team A", "Team B", "Team C"], [30, 2, 20]))
